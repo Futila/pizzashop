0cf1f02dc1759991f46b5b629a74acb826fbede5
Fernando Futila <fernando.futila@gmail.com>
2024-07-19 17:19:47 -0300

    react-helmet-async config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import { HelmetProvider, Helmet } from 'react-helmet-async'
+
 import './global.css'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
