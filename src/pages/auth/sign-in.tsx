0ef077e3b4581d19b1fe5dfe050a2200e633eee8
Fernando Futila <fernando.futila@gmail.com>
2024-07-23 17:52:59 -0300

    feat: add restaurant registration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -4,6 +4,7 @@ import { Label } from '@/components/ui/label'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
@@ -39,7 +40,11 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
+
       <div className="p-8">
+        <Button asChild variant="ghost" className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="jus flex w-[350px] flex-col gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
