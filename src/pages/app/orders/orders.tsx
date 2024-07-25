9e2012c55e41a1d1af9dc3d87741287f510ce284
Fernando Futila <fernando.futila@gmail.com>
2024-07-25 14:40:42 -0300

    feat: add orders filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,15 +1,13 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { OrderTableRow } from './order-table-row'
+import { OrderTableFilters } from './order-table-filters'
 
 export function Orders() {
   return (
@@ -18,12 +16,7 @@ export function Orders() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
 
-        <div className="space-y-2.5">
-          <form className="flex items-center gap-2">
-            <span className="text-sm font-semibold">FIltros</span>
-            <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-          </form>
-        </div>
+        <OrderTableFilters />
 
         <div className="rounded-md border">
           <Table>
@@ -41,46 +34,7 @@ export function Orders() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="h3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-xs font-medium">
-                      23452sdfsdfs23
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      há 15 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      Fernando Futila
-                    </TableCell>
-                    <TableCell className="font-medium">R$ 189,89</TableCell>
-                    <TableCell>
-                      <Button variant="outline">
-                        <ArrowRight className="mr-2 h-3 w-3" />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )
+                return <OrderTableRow key={i} />
               })}
             </TableBody>
           </Table>
