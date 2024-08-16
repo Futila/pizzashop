4b076d7f4e2b2e05c6b3519c8af0caad015c9cf0
Fernando Futila <fernando.futila@gmail.com>
2024-08-16 11:59:29 -0300

    feat: add graphics loading

--- src/pages/app/dashboard/popular-product-chart.tsx
+++ src/pages/app/dashboard/popular-product-chart.tsx
@@ -1,6 +1,6 @@
 import { getPopularProducts } from '@/api/get-popular-products'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { BarChart } from 'lucide-react'
+import { BarChart, Loader2 } from 'lucide-react'
 import { useQuery } from 'react-query'
 
 import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts'
@@ -29,7 +29,7 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {popularProducts && (
+        {popularProducts ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart style={{ fontSize: 40 }}>
               <Pie
@@ -86,6 +86,10 @@ export function PopularProductsChart() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
