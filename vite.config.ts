40f14a844285589cfdcad7985009ff957764d30f
Fernando Futila <fernando.futila@gmail.com>
2024-08-19 13:27:00 -0300

    tests: order status

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
-    enviroment: 'happy-dom',
+    environment: 'happy-dom',
   },
 } as UserConfig & {
   test: InlineConfig
