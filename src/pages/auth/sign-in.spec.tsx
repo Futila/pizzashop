2a3e5dc5a6d59b29dfda70a877093a2218b7442d
Fernando Futila <fernando.futila@gmail.com>
2024-08-23 11:09:54 -0300

    tests: sign in

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react'
+import { SignIn } from './sign-in'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '@/lib/react-query'
+
+describe('SignIn', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=fernando@example.com']}
+            >
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+    expect(emailInput.value).toEqual('fernando@example.com')
+    // expect(emailInput.value).toBe('fernando@example.com')
+  })
+})
